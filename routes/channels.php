f010d4ac54088da9ab0f367704350bc61a5f07dd
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-02 18:58:02 +0300

    Добавить broadcast-канал для авторизованного пользователя

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,6 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('user.{userId}', function (\App\Models\Account\User $user, $userId) {
+    return (integer)$userId === $user->id;
 });
