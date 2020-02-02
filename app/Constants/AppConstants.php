4944e2ff0adc6b592a72fcd0f1cf00cc2dcf33d6
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-02 18:38:32 +0300

    Добавить константы формата времени и для чата

---
+++ app/Constants/AppConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+use Carbon\Carbon;
+
+class AppConstants
+{
+    public const DATE_FORMAT = 'Y-m-d';
+    public const DATETIME_FORMAT = Carbon::DEFAULT_TO_STRING_FORMAT;
+}
---
+++ app/Constants/ChatConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Constants;
+
+class ChatConstants
+{
+    public const MAX_MESSAGE_CONTENT_LENGTH = 1024;
+    public const MAX_MESSAGE_TOKEN_LENGTH = 64;
+}
