1a26abb9b2aec876c974ebb2ae4faf665742f45a
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-02 19:22:30 +0300

    Добавить метод с ответом ошибки в базовый контроллер

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ErrorResource;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendError($code, $message = null, $exception = null)
+    {
+        $response = [
+            'message' => $message ?? Response::$statusTexts[$code],
+        ];
+        if (config('app.debug')) {
+            $response['exception'] = $exception;
+        }
+        if (request()->ajax()) {
+            return response()->json(new ErrorResource($response), $code);
+        }
+        return abort($code, $message);
+    }
 }
